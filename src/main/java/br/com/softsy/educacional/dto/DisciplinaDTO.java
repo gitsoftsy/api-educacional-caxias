23052011d26a068b2a10a3389d5b19d03df11551
RaphaelSoftsy <raphael.lopes@softsy.com.br>
2024-04-29 09:03:43 -0300

    add o campo horasLab
    add o campo horasLab ;
    CLASSE DISCIPLINA

--- src/main/java/br/com/softsy/educacional/dto/DisciplinaDTO.java
+++ src/main/java/br/com/softsy/educacional/dto/DisciplinaDTO.java
@@ -9,38 +9,40 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class DisciplinaDTO {
-    private Long idDisciplina;
-    
-    private DependenciaAdministrativaDTO dependenciaAdmId;
-    
-    private Long escolaId;
-    
-    private String disciplina;
-    private String nome;
-    
-    private Double creditos;
-    
-    private Double horasAula;
-    
-    private Double horasEstagio;
-    
-    private Double horasAtiv;
-    
-    private LocalDateTime dataCadastro;
-    private Character ativo;
-    
-    public DisciplinaDTO(Disciplina disciplina) {
-        this.idDisciplina = disciplina.getIdDisciplina();
-        this.dependenciaAdmId = new DependenciaAdministrativaDTO(disciplina.getDependenciaAdm());
-        this.escolaId = disciplina.getEscola().getIdEscola();
-        this.disciplina = disciplina.getDisciplina();
-        this.nome = disciplina.getNome();
-        this.creditos = disciplina.getCreditos();
-        this.horasAula = disciplina.getHorasAula();
-        this.horasEstagio = disciplina.getHorasEstagio();
-        this.horasAtiv = disciplina.getHorasAtiv();
-        this.dataCadastro = disciplina.getDataCadastro();
-        this.ativo = disciplina.getAtivo();
-    }
-}
+	private Long idDisciplina;
 
+	private DependenciaAdministrativaDTO dependenciaAdmId;
+
+	private Long escolaId;
+
+	private String disciplina;
+	private String nome;
+
+	private Double creditos;
+
+	private Double horasAula;
+
+	private Double horasEstagio;
+
+	private Double horasAtiv;
+
+	private Double horasLab;
+
+	private LocalDateTime dataCadastro;
+	private Character ativo;
+
+	public DisciplinaDTO(Disciplina disciplina) {
+		this.idDisciplina = disciplina.getIdDisciplina();
+		this.dependenciaAdmId = new DependenciaAdministrativaDTO(disciplina.getDependenciaAdm());
+		this.escolaId = disciplina.getEscola().getIdEscola();
+		this.disciplina = disciplina.getDisciplina();
+		this.nome = disciplina.getNome();
+		this.creditos = disciplina.getCreditos();
+		this.horasAula = disciplina.getHorasAula();
+		this.horasEstagio = disciplina.getHorasEstagio();
+		this.horasAtiv = disciplina.getHorasAtiv();
+		this.horasLab = disciplina.getHorasLab();
+		this.dataCadastro = disciplina.getDataCadastro();
+		this.ativo = disciplina.getAtivo();
+	}
+}
--- src/main/java/br/com/softsy/educacional/model/Disciplina.java
+++ src/main/java/br/com/softsy/educacional/model/Disciplina.java
@@ -15,10 +15,8 @@ import javax.persistence.UniqueConstraint;
 import lombok.Data;
 
 @Entity
-@Table(name = "TBL_DISCIPLINA", 
-	uniqueConstraints = { 
-		@UniqueConstraint(name = "UQ_DISCIPLINA", columnNames = { "DISCIPLINA", "ID_DEPENDENCIA_ADMINISTRATIVA" })
-		})
+@Table(name = "TBL_DISCIPLINA", uniqueConstraints = {
+		@UniqueConstraint(name = "UQ_DISCIPLINA", columnNames = { "DISCIPLINA", "ID_DEPENDENCIA_ADMINISTRATIVA" }) })
 @Data
 public class Disciplina {
 
@@ -26,36 +24,40 @@ public class Disciplina {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID_DISCIPLINA")
 	private Long idDisciplina;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "ID_DEPENDENCIA_ADMINISTRATIVA", nullable = false)
 	private DependenciaAdministrativa dependenciaAdm;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "ID_ESCOLA")
 	private Escola escola;
-	
+
 	@Column(name = "DISCIPLINA", nullable = false, unique = true, length = 15)
 	private String disciplina;
-	
+
 	@Column(name = "NOME", nullable = false)
 	private String nome;
-	
+
 	@Column(name = "CREDITOS")
 	private Double creditos;
-	
+
 	@Column(name = "HORAS_AULA")
 	private Double horasAula;
-	
+
 	@Column(name = "HORAS_ESTAGIO")
 	private Double horasEstagio;
-	
+
 	@Column(name = "HORAS_ATIV")
 	private Double horasAtiv;
-	
+
+	@Column(name = "HORAS_LAB")
+	private Double horasLab;
+
 	@Column(name = "DT_CADASTRO", columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
 	private LocalDateTime dataCadastro;
 
 	@Column(name = "ATIVO", nullable = false)
 	private Character ativo;
+
 }
