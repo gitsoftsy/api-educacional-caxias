a2e20366eff5e99dd765dcdcb78fbf91b261f9f0
PaulaBlesaSoft <paula.blesa@softsy.io>
2024-12-09 15:58:03 -0300

    COMECANDO TBL_PROVA

---
+++ src/main/java/br/com/softsy/educacional/dto/ProvaDTO.java
@@ -0,0 +1,5 @@
+package br.com.softsy.educacional.dto;
+
+public class ProvaDTO {
+
+}
---
+++ src/main/java/br/com/softsy/educacional/model/Prova.java
@@ -0,0 +1,66 @@
+package br.com.softsy.educacional.model;
+
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "TBL_PROVA")
+@Data
+public class Prova {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_PROVA")
+    private Long idProva;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_TURMA", nullable = false)
+    private Turma idTurma;
+
+    @Column(name = "DT_CADASTRO", columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime dataCadastro;
+
+    @Column(name = "NOME_ABREVIADO", length = 5, nullable = false)
+    private String nomeAbreviado;
+
+    @Column(name = "DESCRICAO", length = 55, nullable = false)
+    private String descricao;
+
+    @Column(name = "DT_DIVULGACAO")
+    private LocalDate dataDivulgacao;
+
+    @Column(name = "DT_AGENDA_PROVA")
+    private LocalDateTime dataAgendaProva;
+
+    @Column(name = "ATIVO", length = 1, nullable = false)
+    private Character ativo;
+
+    @Column(name = "ORDEM")
+    private Integer ordem;
+
+    @Column(name = "TIPO_CONCEITO", length = 1, nullable = false)
+    private Character tipoConceito;
+
+    @Column(name = "CONCEITO_MAX", length = 10)
+    private String conceitoMax;
+
+    @Column(name = "DT_LIMITE_REVISAO")
+    private LocalDate dataLimiteRevisao;
+
+    @Column(name = "EH_SIMULADO", length = 1, nullable = false)
+    private Character ehSimulado;
+
+    @Column(name = "FORMULA", length = 255)
+    private String formula;
+}
